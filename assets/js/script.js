8fad3964b2a07e075166aec59a7b21816e2bafb3
Cadenza <mirandalucsb@gmail.com>
2021-12-03 12:41:00 -0700

    added some code to skeletons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-//api key 50f3b7bbc39633c0695f5b6b710ca954
-//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+//api key 
+//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=hourly,minutely,current,alerts&appid=50f3b7bbc39633c0695f5b6b710ca954
 
 //need to get the lat and lon of the city the user types in.(How?)
 //Need to figure out which data to exclude 
@@ -9,21 +9,37 @@
 //need to generate html elements for the weather for the next five days.
 
 var formEl = document.querySelector(".weather-form")
+//records search history
+var searchHistory = [];
+
+//used to hold the weather data for one day
+class day {
+
+}
+
+//used to hold weather data for the current day and five future days
+class weather{
+    today;
+    futureDays = [];
+}
+
 
 var getWeatherData = function(){
-
+    //the fetch request goes here! puts data into weather object.
 }
 
 var displyWeatherData = function(){
-
+    //takes the data from the weather object and makes html elements to display it 
 }
 
 var getCityLatLon = function(){
-
+    //get the city latitude nad longitude from the city name
+    lat = Geolocation.
+    return {lat, lon};
 }
 
 var addCityToSearchHistory = function(){
-
+    //adds city name to list of searches.
 }
 
 formEl.addEventListener("submit", function(event){
