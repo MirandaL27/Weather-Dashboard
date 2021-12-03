0c8cf01f957951c8ad62aac4f490a46bc08c053c
Cadenza <mirandalucsb@gmail.com>
2021-12-03 16:25:10 -0700

    added fetch requests

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 //api key 
 //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=hourly,minutely,current,alerts&appid=50f3b7bbc39633c0695f5b6b710ca954
-
+//http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit=1&appid=50f3b7bbc39633c0695f5b6b710ca954
 //need to get the lat and lon of the city the user types in.(How?)
 //Need to figure out which data to exclude 
 //Need to make a form for the user - input for city, search button
@@ -24,20 +24,51 @@ class weather{
 }
 
 
-var getWeatherData = function(){
+var getWeatherData = function(lat, lon){
     //the fetch request goes here! puts data into weather object.
+    var weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat +"&lon=" +lon+ "&exclude=hourly,minutely,current,alerts&appid=50f3b7bbc39633c0695f5b6b710ca954";
+    fetch(weatherUrl)
+    .then(function(response){
+    if(response.ok){
+        response.json().then(function (data){
+        console.log(data);
+        displyWeatherData();
+    });
+    }else{
+        alert('Error: ' + response.statusText);
+    }
+    })
+    .catch(function (error) {
+        alert('Unable to connect to Open Weather API');
+      });
 }
 
 var displyWeatherData = function(){
     //takes the data from the weather object and makes html elements to display it 
 }
 
-var getCityLatLon = function(){
+var getCityLatLon = function(cityName){
     //get the city latitude nad longitude from the city name
-    var lat = 0;//get latitude here
-    var lon = 0;//get longitude here
-    var coord = {lat,lon};
-    return coord;
+    //need a fetch request for the data from the weather api
+    var weatherUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=50f3b7bbc39633c0695f5b6b710ca954"
+    var lat = 0;
+    var lon = 0;
+    fetch(weatherUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+            lat = data[0].lat;
+            lon = data[0].lon;
+            getWeatherData(lat,lon);
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert('Unable to connect to Open Weather API');
+    });
+    
 }
 
 var displayCitySearchHistory = function() {
@@ -51,7 +82,9 @@ var addCityToSearchHistory = function(){
 
 formEl.addEventListener("submit", function(event){
     event.preventDefault();
-    console.log("search button clicked");
+    var inputEl = document.querySelector("#city");
+    //getWeatherData(inputEl.value);
+    getCityLatLon(inputEl.value);
 });
 
 //need to add an event listener for if a city in the search history is clicked.
