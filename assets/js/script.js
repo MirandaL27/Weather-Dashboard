91d100ab5b35e229ee043a8a18b7e795b10d8221
Cadenza <mirandalucsb@gmail.com>
2021-12-03 10:56:30 -0700

    initial code and comments

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//api key 50f3b7bbc39633c0695f5b6b710ca954
+//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+//need to get the lat and lon of the city the user types in.(How?)
+//Need to figure out which data to exclude 
+//Need to make a form for the user - input for city, search button
+//need a search history where the cities are clickable and show current weather data
+//need to generate html elements for the weather for today
+//need to generate html elements for the weather for the next five days. 
