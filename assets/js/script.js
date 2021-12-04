cd2e60aa3f4b5349a184ade7336b636ca47cd455
Cadenza <mirandalucsb@gmail.com>
2021-12-04 15:57:09 -0700

    created html elements for data from weather app

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,7 @@
-//api key 
+
 //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=hourly,minutely,current,alerts&appid=50f3b7bbc39633c0695f5b6b710ca954
 //http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit=1&appid=50f3b7bbc39633c0695f5b6b710ca954
-//need to get the lat and lon of the city the user types in.(How?)
-//Need to figure out which data to exclude 
-//Need to make a form for the user - input for city, search button
 //need a search history where the cities are clickable and show current weather data
-//need to generate html elements for the weather for today
 //need to generate html elements for the weather for the next five days.
 
 var formEl = document.querySelector(".weather-form")
@@ -14,24 +10,48 @@ var searchHistory = [];
 
 //used to hold the weather data for one day
 class day {
-
+    cityName;
+    temp;
+    wind;
+    humidity;
+    uvi;
+    uviSeverity;
+    icon;
+    date;
 }
 
 //used to hold weather data for the current day and five future days
-class weather{
-    today;
-    futureDays = [];
-}
+var days = [];
 
 
-var getWeatherData = function(lat, lon){
+var getWeatherData = function(lat, lon, city){
     //the fetch request goes here! puts data into weather object.
-    var weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat +"&lon=" +lon+ "&exclude=hourly,minutely,current,alerts&appid=50f3b7bbc39633c0695f5b6b710ca954";
+    var weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat +"&lon=" +lon+ "&units=imperial"+"&exclude=hourly,minutely,current,alerts&appid=50f3b7bbc39633c0695f5b6b710ca954";
     fetch(weatherUrl)
     .then(function(response){
     if(response.ok){
         response.json().then(function (data){
-        console.log(data);
+        for(var i = 0; i < 6 ; i++){
+            var testDay = new day();
+            testDay.cityName = city;
+            testDay.temp = data.daily[i].temp.day;
+            testDay.wind = data.daily[i].wind_speed;
+            testDay.humidity = data.daily[i].humidity;
+            testDay.uvi = data.daily[i].uvi;
+            testDay.icon = data.daily[i].weather[0].icon;
+            var date = new Date(data.daily[i].dt*1000);
+            testDay.date = date.toLocaleDateString("en-US");
+            if(testDay.uvi <= 2 && testDay.uvi >= 0){
+                testDay.uviSeverity = "Low";
+            }
+            else if(testDay.uvi >=3 && testDay.uvi <=7){
+                testDay.uviSeverity = "Moderate";
+            }
+            else{
+                testDay.uviSeverity = "Severe";
+            }
+            days.push(testDay);
+        }   
         displyWeatherData();
     });
     }else{
@@ -43,8 +63,70 @@ var getWeatherData = function(lat, lon){
       });
 }
 
+var getUIVBackgroundColor = function(severity){
+    if(severity === "Low"){
+        return "green";
+    }
+    else if(severity === "Moderate"){
+        return "yellow";
+    }
+    else{
+        return "red";
+    }
+}
+var displayToday = function(){
+    var divEl = document.querySelector(".today");
+    divEl.style.display = "block";
+    var h2El = document.createElement("h2");
+    h2El.textContent = days[0].cityName + " (" + days[0].date + ")";
+    divEl.appendChild(h2El);
+
+    varIconUrl = "http://openweathermap.org/img/wn/" + days[0].icon + "@2x.png";
+    var imgEl = document.createElement("img");
+    imgEl.src = varIconUrl;
+    divEl.appendChild(imgEl);
+
+    var tempEl = document.createElement("p");
+    tempEl.textContent = "Temp: "+days[0].temp + "\xB0F";
+    divEl.appendChild(tempEl)
+
+    var windEl = document.createElement("p");
+    windEl.textContent = "Wind: " + days[0].temp + " MPH";
+    divEl.appendChild(windEl);
+
+    var humidityEl = document.createElement("p");
+    humidityEl.textContent = "Humidity: " + days[0].humidity + " %";
+    divEl.appendChild(humidityEl);
+
+    var uviEl = document.createElement("p");
+    uviEl.textContent = "UV Index: ";
+    var uviSpan = document.createElement("span");
+    uviSpan.textContent = days[0].uvi;
+    uviSpan.style.backgroundColor = getUIVBackgroundColor(days[0].uviSeverity);
+    uviSpan.style.color = "white";
+    uviSpan.style.padding = "5px";
+    uviSpan.style.borderRadius = "10px";
+    uviEl.appendChild(uviSpan);
+    divEl.appendChild(uviEl);
+}
+
 var displyWeatherData = function(){
-    //takes the data from the weather object and makes html elements to display it 
+    //takes the data from the weather array and makes html elements to display it 
+    //make first day
+    displayToday();
+
+    for(var i = 1; i<6;i++){
+        //makes the other five days
+        var divEl = document.querySelector(".other-five-days");
+        var childDivEl = document.createElement("div");
+        childDivEl.style.backgroundColor = "blue";
+        childDivEl.style.color = "white";
+        var h3El = document.createElement("h3");
+        h3El.textContent = days[i].date;
+        childDivEl.appendChild(h3El);
+
+        divEl.appendChild(childDivEl);
+    }
 }
 
 var getCityLatLon = function(cityName){
@@ -59,7 +141,7 @@ var getCityLatLon = function(cityName){
         response.json().then(function (data) {
             lat = data[0].lat;
             lon = data[0].lon;
-            getWeatherData(lat,lon);
+            getWeatherData(lat,lon, cityName);
         });
       } else {
         alert('Error: ' + response.statusText);
@@ -83,7 +165,6 @@ var addCityToSearchHistory = function(){
 formEl.addEventListener("submit", function(event){
     event.preventDefault();
     var inputEl = document.querySelector("#city");
-    //getWeatherData(inputEl.value);
     getCityLatLon(inputEl.value);
 });
 
